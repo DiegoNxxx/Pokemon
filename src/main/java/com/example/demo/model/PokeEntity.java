fb9124a0aebca26415b53fd0c8e3eea1c2e9829d
Javier272 <147571233+Javier272@users.noreply.github.com>
2026-04-15 22:34:09 -0600

    1

---
+++ src/main/java/com/example/demo/model/PokeEntity.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+public class PokeEntity {
+    private int pokemon1vida;
+    private int pokemon2vida;
+
+    public int getPokemon1Vida(){
+        return pokemon1vida;
+    }
+
+    public int getPokemon2Vida(){
+        return pokemon2vida;
+    }
+
+    public void setPokemon1Vida(int pokemon1vida) {
+        this.pokemon1vida = pokemon1vida;
+    }
+
+    public void setPokemon2Vida(int pokemon2vida){
+        this.pokemon2vida = pokemon2vida;
+    }
+}
--- src/main/java/com/example/demo/services/PokeConsultasService.java
+++ src/main/java/com/example/demo/services/PokeConsultasService.java
@@ -1,5 +1,54 @@
 package com.example.demo.services;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import com.example.demo.model.PokeEntity;
+import com.example.demo.model.Pokemon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import jakarta.annotation.PostConstruct;
+
+import com.google.firebase.FirebaseApp;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.cloud.FirestoreClient;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+
+import java.util.concurrent.ExecutionException;
+
+@Service
 public class PokeConsultasService {
-    
+
+    public PokeEntity getVidaPokemons() throws InterruptedException, ExecutionException {
+        Firestore db = FirestoreClient.getFirestore();
+
+        // .get() devuelve un ApiFuture<DocumentSnapshot>
+        // Es decir: una promesa de que el documento llegará en el futuro
+        DocumentSnapshot docSnapshot = db.collection("pokebattles")
+                                        .document("battle1")
+                                        .get()
+                                        .get(); // get() bloquea hasta obtener el documento
+
+        // Verifica si el documento existe en la base de datos
+        if (docSnapshot.exists()) {
+            return docSnapshot.toObject(PokeEntity.class);
+        } else {
+            return new PokeEntity(); // devolver valores por defecto si no existe
+        }
+    }
+
+    public Pokemon savePokemon(Pokemon pokemon) throws InterruptedException, ExecutionException{
+        Firestore db = FirestoreClient.getFirestore();
+
+        DocumentReference docRef = db.collection("pokemones").document();
+
+        String id = docRef.getId();
+        pokemon.setId(id);
+        docRef.set(pokemon).get();
+        return pokemon;
+
+    }
 }
